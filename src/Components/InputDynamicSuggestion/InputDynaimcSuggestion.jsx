2ce66ee0add3aa420a3a112cb87f778b8fec579c
Phil <pnicolet@uwo.ca>
2019-06-16 15:51:22 -0400

    Input Dynamic V1 Complete

--- src/Components/InputDynamicSuggestion/InputDynaimcSuggestion.jsx
+++ src/Components/InputDynamicSuggestion/InputDynaimcSuggestion.jsx
@@ -2,34 +2,54 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types'
 import styles from "./InputDynamicSuggestion.module.css";
 
+const escapeSpaces = (text) => {
+    return text.replace(/ /g, '/');
+}
+
+const handle = "@rbc.com"
+
 class InputDynamicSuggestion extends Component {
   constructor(props) {
     super(props);
     const { initialPlaceHolder } = this.props;
     this.state = {
       placeholder: initialPlaceHolder,
-      textValue: ""
+      textValue: "",
     };
+    this.inputRef = React.createRef();
   }
+
 
   onValueChange = event => {
     const val = event.target.value;
     const { initialPlaceHolder } = this.props;
-    const placeholder = val ? "@rbc.com" : initialPlaceHolder;
+    let placeholder = val ? handle : initialPlaceHolder;
+    if (val && val.includes("@")){
+        const index = val.indexOf("@");
+        const extension = val.substring(index);
+        if (handle.includes(extension)){
+            placeholder = placeholder.replace(extension, "");
+        } else {
+            placeholder = "";
+        }
+    }
     this.setState({
       textValue: val,
       placeholder
     });
   };
 
+  focusOnInput = () => {
+      this.inputRef.current.focus();
+  }
+
   render() {
-    
     const { placeholder, textValue } = this.state;
     return (
       <div className={styles.main}>
-        <div className={styles.text}>{textValue}</div>
-        <div className={styles.placehold}>{placeholder}</div>
-        <input className={styles.input} value={textValue} onChange={this.onValueChange} />
+        <div className={styles.text}>{escapeSpaces(textValue)}</div>
+        <input className={styles.dumbyInput} value={placeholder} onFocus={this.focusOnInput}/>
+        <input className={styles.input} ref={this.inputRef} value={textValue} onChange={this.onValueChange} />
       </div>
     );
   }
